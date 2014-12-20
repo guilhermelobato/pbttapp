59b861ac99cb7d932e401a721da87d94776e6a1d
tiagojco <tiagojco@gmail.com>
2014-12-19 21:49:57 -0300

    removido link das imagem 'zoomables' do corpo da noticia

--- client/templates/noticia.js
+++ client/templates/noticia.js
@@ -37,25 +37,25 @@ Template.corpoDaNoticia.helpers({
 		var fix = html;
 		fix = fix.replace("src=\"/","src=\"http://www.paraibatotal.com.br/");
 		fix = fix.replace("src=\"static/","src=\"http://www.paraibatotal.com.br/static/");
-		fix = fix.replace("href=\"/","href=\"http://www.paraibatotal.com.br/");
 
 		fix = fix.replace("src='/","src='http://www.paraibatotal.com.br/");
 		fix = fix.replace("src='static/","src='http://www.paraibatotal.com.br/static/");
-		fix = fix.replace("href='/","href='http://www.paraibatotal.com.br/");
 
 		//ADICAO DA CLASSE THUMBNAIL E DO WRAPPER
 		fix = $("<div id='renderedCorpo'/>").html(fix);
 
-		//DESENCALACRANDO
-		//fix = fix[0].outerHTML;
-		textnodes = getTextNodesIn(fix[0]);
+		//REMOCAO DO LINK DAS IMAGENS .zoomable
+		$(".zoomable",fix).attr("href","#");
 
+		//REENVOLVENDO TEXTO PURO EM <p/>
+		textnodes = getTextNodesIn(fix[0]);
 		for(var i=0; i < textnodes.length; i++){
 			if($(textnodes[i]).parent().is("#renderedCorpo")){
 				$(textnodes[i]).wrap("<p>");
 			}
 		}
 
+		//CLASSIFICANDO IMAGENS
 		$(".anexo-img.FO",fix).addClass("media-object");
 
 		//RETORNO
